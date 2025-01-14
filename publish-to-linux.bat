2bde07ff7c0b434ea4d5ce100fadd72ad424f1cd
argo-rich <rich@argohaus.com>
2025-01-14 12:37:25 -0600

    LTH-5 - Adding publish-to-linux.bat to deploy file to server

---
+++ publish-to-linux.bat
@@ -0,0 +1,12 @@
+REM Run the dotnet publish command and create publish.tar.bz2
+cd LearningToHunt
+dotnet publish -o publish --os linux
+cd publish
+tar cjvf ..\publish.tar.bz2 .
+
+REM scp publish.tar.bz2 to the server
+cd ..
+scp -i "%L2H_KEY%" .\publish.tar.bz2 %L2H_USER%@%L2H_SERVER%:~/learning-to-hunt
+
+REM run the deploy script on the server
+ssh -i "%L2H_KEY%" %L2H_USER%@%L2H_SERVER% "source /home/%L2H_USER%/.bash_profile && /home/%L2H_USER%/scripts/deploy-publish-file.sh"
