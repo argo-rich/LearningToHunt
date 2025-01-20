1b7982961af4394e8ddad83b53b8b446e7dce6de
argo-rich <rich@argohaus.com>
2025-01-20 16:26:27 -0600

    LTH-75 - Added ability to have anchor link functionality

--- LearningToHunt/ClientApp/src/app/article/article.component.spec.ts
+++ LearningToHunt/ClientApp/src/app/article/article.component.spec.ts
@@ -8,16 +8,7 @@ import { ArticleService } from './article.service';
 import { of } from 'rxjs';
 import { Observable } from 'rxjs';
 import { Article } from '../models/article';
-
-let articleZero: Article = {
-  articleId: 0,
-  title: "",
-  subtitle: "",
-  content: "",
-  authorId: 0,
-  createDate: new Date("0000-00-00T00:00:00"),
-  modifyDate: new Date("0000-00-00T00:00:00")
-};
+import { DOCUMENT } from '@angular/common';
 
 let articles = new Map<number, Article>([
   [1, {
@@ -43,7 +34,7 @@ let articles = new Map<number, Article>([
 // Mock ArticleService
 class MockArticleService {
   getArticle(id: number): Observable<Article> {
-    return of(articles.get(id) || articleZero);
+    return of(articles.get(id)!);
   }
 
   updateArticle(id: number, updatedArticle: Article): Observable<void> {
@@ -58,7 +49,7 @@ const mockActivatedRoute = {
   snapshot: { 
     paramMap: {
       get(id: string): string {        
-        return '1';
+        return '1'; // mocking 'articleId' of '1'
       }
     }
   }
@@ -75,6 +66,7 @@ describe('ArticleComponent', () => {
       providers: [
         { provide: ArticleService, useClass: MockArticleService },
         { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Document, useExisting: DOCUMENT },
         provideHttpClient()
       ]
     })
@@ -88,16 +80,16 @@ describe('ArticleComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); 
   });
   
   it('init should retreive article1 from ArticleService', () => {
-    expect(component.article).toBe(articles.get(1) || articleZero);
+    expect(component.article).toBe(articles.get(1)!);
   });
 
   it('should retreive article2 from ArticleService', () => {
     component.getArticle(2);
-    expect(component.article).toBe(articles.get(2) || articleZero);
+    expect(component.article).toBe(articles.get(2)!);
   });
 
   it('should update article1 via ArticleService', () => {
@@ -107,9 +99,38 @@ describe('ArticleComponent', () => {
         content: "Updated content."
     });
     component.updateArticle();
-    expect(component.article).toBe(articles.get(1) || articleZero);
+    expect(component.article).toBe(articles.get(1)!);
     expect(component.article.title).toBe("Updated Title");
-    expect((articles.get(1) || articleZero).subtitle).toBe("Updated Subtitle");
+    expect((articles.get(1)!).subtitle).toBe("Updated Subtitle");
+  });
+
+  it('setUpAnchorLinks() should add the anchor links', () => {
+    // arrange    
+    let appRoot = document.createElement("app-root");
+    let appArticle = document.createElement("app-article");
+
+    appRoot.appendChild(appArticle);
+    document.body.appendChild(appRoot);
+
+    let anchorLinksDiv = document.createElement("div");
+    anchorLinksDiv.className = "anchor-links";
+    appArticle.appendChild(anchorLinksDiv);
+
+    let heading1 = document.createElement("h2");
+    heading1.textContent = "Hunter Education";
+    heading1.className = "anchor-point";
+    appArticle.appendChild(heading1);
+
+    let heading2 = document.createElement("h2");
+    heading2.textContent = "Hunting Laws";
+    heading2.className = "anchor-point";
+    appArticle.appendChild(heading2);
+    
+    // act
+    component.setUpAnchorLinks();
+    
+    // assert
+    expect(document.getElementsByClassName("anchor-link").length).toBe(2);
   });
 
 });
--- LearningToHunt/ClientApp/src/app/article/article.component.ts
+++ LearningToHunt/ClientApp/src/app/article/article.component.ts
@@ -1,5 +1,5 @@
 //default
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 
 //added
 import { Article } from '../models/article';
@@ -8,6 +8,7 @@ import { ActivatedRoute } from '@angular/router';
 import { FormsModule, ReactiveFormsModule, FormGroup, Validators, FormControl } from '@angular/forms';
 import {NgIf} from '@angular/common';
 import { AngularEditorModule } from '@kolkov/angular-editor';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-article',
@@ -16,9 +17,10 @@ import { AngularEditorModule } from '@kolkov/angular-editor';
   styleUrl: './article.component.css'
 })
 
-export class ArticleComponent implements OnInit {
+export class ArticleComponent implements OnInit, AfterViewInit {
   
   editMode: boolean = false;
+  articleLoaded: boolean = false;
   article: Article = {
     "articleId": 0,
     "title": "",
@@ -32,9 +34,10 @@ export class ArticleComponent implements OnInit {
 
   constructor(
     private articleService: ArticleService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    @Inject(DOCUMENT) document: Document
   ) {}
-
+  
   ngOnInit(): void {
     const articleId = parseInt(this.route.snapshot.paramMap.get('articleId') || "0");
     if (articleId !== 0) {
@@ -42,7 +45,20 @@ export class ArticleComponent implements OnInit {
     } 
   }
 
-  get title() {
+  async ngAfterViewInit(): Promise<void> {
+    while (!this.articleLoaded) {
+      console.log("It's not loaded.");
+      await this.delay(20);
+    }
+    await this.delay(20);
+    this.setUpAnchorLinks();
+  }
+
+  async delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  get title() {    
     return this.articleForm.get('title');
   }
 
@@ -63,19 +79,64 @@ export class ArticleComponent implements OnInit {
         subtitle: new FormControl(this.article.subtitle, [Validators.required, Validators.maxLength(35)]),
         content: new FormControl(this.article.content, [Validators.required])
       });
+      this.articleLoaded = true;
     });
   }
 
   updateArticle() {
     let id = parseInt(this.route.snapshot.paramMap.get('articleId') || "0");
-    let article = this.articleForm.value;
-    
+
     this.editMode = false;
     this.article.title = this.title?.value;
     this.article.subtitle = this.subtitle?.value;
     this.article.content = this.content?.value;
     this.articleService.updateArticle(id, this.article).subscribe(() => {
       console.log("Update request processed");
-    });;
+      this.setUpAnchorLinks();
+    });
+  }
+
+  async cancel(): Promise<void> {
+    this.editMode = false;
+    await this.delay(20);
+    this.setUpAnchorLinks();
+  }
+  
+  /**
+   * Dynamically sets up anchor links by looking for an empty <div> with a class name of anchor-links.
+   * If found, it further looks for any <h2>'s with a class name of anchor-point and builds
+   * link-styled buttons with a click function which calls scrollTo to scroll to the appropriate
+   * <h2> location.
+   * 
+   * @remarks
+   * This was done because 1) Angular does not handle anchor links very well and 2) does not document
+   * a way to use them with content originating from an HttpClient call.
+   */
+  setUpAnchorLinks() {
+    // get the anchorLinksUl element to later attach things to
+    let anchorLinksDiv;
+    let elements = document.querySelectorAll('div.anchor-links');
+    
+    for (let element of elements) {
+      anchorLinksDiv = element;
+      break;
+    }
+
+    if (anchorLinksDiv != null) {
+      let anchorPoints = document.querySelectorAll('h2.anchor-point');
+
+      // iterate through the anchorPoints and add them to anchor-links div
+      for (let element of anchorPoints) {
+        let button = document.createElement("button");
+        let anchorPoint = element.getBoundingClientRect().top;
+        let func = function() { scrollTo({top: anchorPoint, left: 0, behavior: 'smooth'}); };
+        button.innerText = element.textContent!;
+        button.className = "btn btn-link anchor-link";
+        button.addEventListener("click", func, false);
+  
+        anchorLinksDiv.appendChild(button);
+        anchorLinksDiv.appendChild(document.createElement("br"));
+      }
+    }
   }
 }
