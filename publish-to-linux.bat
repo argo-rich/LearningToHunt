033e4b63d06885b008f6a353c1c0499756dbfb4a
argo-rich <rich@argohaus.com>
2025-02-04 17:17:02 -0600

    LTH-131 Cleaning out previous builds to prevent old items from getting into production.

--- publish-to-linux.bat
+++ publish-to-linux.bat
@@ -1,5 +1,7 @@
 REM Run the dotnet publish command and create publish.tar.bz2
 cd LearningToHunt
+dotnet clean
+rmdir publish -Recurse
 dotnet publish -o publish --os linux
 cd publish
 tar cjvf ..\publish.tar.bz2 .
@@ -7,6 +9,7 @@ tar cjvf ..\publish.tar.bz2 .
 REM scp publish.tar.bz2 to the server
 cd ..
 scp -i "%L2H_KEY%" .\publish.tar.bz2 %L2H_USER%@%L2H_SERVER%:~/learning-to-hunt
+del .\publish.tar.bz2
 
 REM run the deploy script on the server
 ssh -i "%L2H_KEY%" %L2H_USER%@%L2H_SERVER% "source /home/%L2H_USER%/.bash_profile && /home/%L2H_USER%/scripts/deploy-publish-file.sh"
